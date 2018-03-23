1cd14687fa9d17b78cd95a7c56ef32f181e8002e
Sivakumar Shanmugavelu <sivakumar.shanmugavelu@boeing.com>
2018-03-23 13:50:34 +0530

    initial check in

---
+++ DemoClient/Controllers/ClientController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Pivotal.Discovery.Client;
+//using Steeltoe.Common.Discovery;
+
+namespace DemoClient.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Client")]
+    public class ClientController : Controller
+    {
+        private readonly IDiscoveryClient _discoveryClient;
+        private readonly HttpClient _httpClient;
+        private string ServiceURL;
+
+        public ClientController(IDiscoveryClient client, IConfiguration config)
+        {
+            _discoveryClient = client;
+            _httpClient = new HttpClient(new DiscoveryHttpClientHandler(_discoveryClient));
+            ServiceURL = config.GetSection("ServiceConfig:URI").Value;
+        }
+
+        public async Task<string> Get()
+        {
+            var response = await _httpClient.GetAsync($"{ServiceURL}/api/Values");
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ DemoService/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Steeltoe.Extensions.Configuration;
+using Steeltoe.Extensions.Configuration.CloudFoundry;
+
+namespace DemoService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseCloudFoundryHosting()
+                .ConfigureAppConfiguration((builderContext, configBuilder) =>
+                {
+                    var env = builderContext.HostingEnvironment;
+                    configBuilder.SetBasePath(env.ContentRootPath)
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                        .AddEnvironmentVariables()
+                        .AddCloudFoundry();
+                })
+                .Build();
+    }
+}
